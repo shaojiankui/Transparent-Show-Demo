acff7c38a700fd89a803ce8151387c1e6ecdf885
Jakey <i@skyfox.org>
2014-08-07 13:02:24 +0800

    Initial Commit

---
+++ Transparent-Show-Demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Transparent-Show-Demo
+//
+//  Created by Jakey on 14-8-7.
+//  Copyright (c) 2014年 www.skyfox.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
